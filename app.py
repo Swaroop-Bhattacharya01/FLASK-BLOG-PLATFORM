ad74f5c44fed9e9cf89aa36fd14dea461eebbece
Swaroop Bhattacharya <swaroop.bhattacharya01@gmail.com>
2025-12-30 14:10:11 +0530

    registration form and user validation

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm,LoginForm #we import the forms we created in forms.py so that we can use them in out routes
 app=Flask(__name__)
 
+app.config['SECRET_KEY']='bbe58e27522d0f25802798769c99e8b1' #config is used to set secret key for forms because it is required for security purposes
 
 posts=[
     {
@@ -25,6 +27,20 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'account created for {form.username.data}','success') #success is a category for the flash message
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+@app.route('/login')
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='Login',form=form)
+
 
 if __name__=="__main__":
     app.run(debug=True)
