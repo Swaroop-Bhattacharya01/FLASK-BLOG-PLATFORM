389e0ca7eed486c86281a815f7671017f96d5ecd
Swaroop Bhattacharya <swaroop.bhattacharya01@gmail.com>
2026-01-02 18:58:18 +0530

    started creating package structure

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from datetime import datetime
+
 from flask import Flask,render_template,url_for,flash,redirect
 from forms import RegistrationForm,LoginForm #we import the forms we created in forms.py so that we can use them in out routes
 from flask_sqlalchemy import SQLAlchemy
+
+
 app=Flask(__name__)
 
 
@@ -11,27 +13,10 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 #creating a database instance
 db=SQLAlchemy(app)
 
-class User(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(20),unique=True,nullable=False)
-    email=db.Column(db.String(120),unique=True,nullable=False)
-    image_file=db.Column(db.String(20),nullable=False,default='default.jpeg')
-    password=db.Column(db.String(60),nullable=False)
-    posts=db.relationship('Post',backref='author',lazy=True) # we use this to create relationship between user and post like one to many relationship and the backref is used to add a column tto the post table which will reference the user who created the post and lazy is used to tell sqlalchemy when to load the data from the database
-    
-    
-    def __repr__(self):
-        return f"user('{self.username},'{self.email}','{self.image_file}')"
-    
-class Post(db.Model):
-        id=db.Column(db.Integer,primary_key=True)
-        title=db.Column(db.String(100),nullable=False)
-        date_posted=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
-        content=db.Column(db.Text,nullable=False)
-        user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+from models import User,Post #the reason this is after the db instance is created is because models.py uses the db instance to create the models
 
-        def __repr__(self):
-            return f"Post('{self.title}','{self.date_posted}')"
+
+
 posts=[
     {
         'author':'corey schafer',
